afb23ad950b0d13a21414e14f16ad6eeab0669a9
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2025-12-11 19:03:34 +0300

    Bug fix and script enhanced
    Will also implement the page for adding segmentation groups to campaigns soon.

--- app/services/CampaignExecutionService.py
+++ app/services/CampaignExecutionService.py
@@ -6,10 +6,25 @@ from app.models.CampaignModel import CampaignModel
 from app.models.WorkflowModel import WorkflowModel
 from app.models.WorkflowStepModel import WorkflowStepModel
 from app.repositories.CampaignRepository import CampaignRepository
+from app.repositories.SegmentationRepository import SegmentationRepository
 
 class CampaignExecutionService
 
     # will try and execute the relevant workflow step, no exception handling here
     @staticmethod
     def executeWorkflowStep(step: WorkflowStepModel):
-        return true
+        CHECK(action_type IN ('email', 'discord-post', 'discount')),
+        if step.action_type == "email":
+            #send email with relevant payload
+        elif step.action_type == "discord-post":
+            #post discord post through webhook
+        elif step.action_type == "discount":
+            #register discount to segments related to campaign which this workflow belongs
+            discount = int(step.action_payload)
+            #get relevant segmentation groups
+            groups = CampaignRepository.getSegmentsFromWorkflowStep(step.id)
+            for group in groups:
+                SegmentationRepository.addSegmentationDiscount(group.id, action_payload)
+
+    @staticmethod
+    def
